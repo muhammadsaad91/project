3f97af8487656f45a45e3abca1112924be349373
muhammadsaad91 <78014984+muhammadsaad91@users.noreply.github.com>
2021-05-27 21:08:32 +0500

    Add files via upload
    flag on c languaGE RUN ON TURBO C

---
+++ flag.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+struct student 
+{
+	char name[15];
+	
+	int roll ;
+};
+int main()
+{
+	struct student stu1;
+	
+	printf("Enter student name::");
+	scanf("%s",&stu1.name);
+	printf("Enter roll number::");
+	scanf("%d",&stu1.roll);
+	
+	printf("%-10s%10s", "NAME","ROLL NUMBER\n");
+	printf("%-10s%10d\n",stu1.name,stu1.roll);
+	return 0;
+}
